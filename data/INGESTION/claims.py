266c453576258f9bc6b08244e3ff3f9716eec983
Prateek07-Kumar <prateekchaurasia791@gmail.com>
2026-02-17 12:46:45 +0530

    update-claims-code

--- data/INGESTION/claims.py
+++ data/INGESTION/claims.py
@@ -7,9 +7,9 @@ spark = SparkSession.builder \
                     .getOrCreate()
 
 # configure variables
-BUCKET_NAME = "healthcare-bucket-22032025"
+BUCKET_NAME = "healthcare-bucket-221001"
 CLAIMS_BUCKET_PATH = f"gs://{BUCKET_NAME}/landing/claims/*.csv"
-BQ_TABLE = "avd-databricks-demo.bronze_dataset.claims"
+BQ_TABLE = "metal-vehicle-482506-n5.bronze_dataset.claims"
 TEMP_GCS_BUCKET = f"{BUCKET_NAME}/temp/"
 
 # read from claims source
