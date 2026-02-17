2eca16218d06f27e107e8612c4f008bee5955a97
Prateek07-Kumar <prateekchaurasia791@gmail.com>
2026-02-17 23:00:43 +0530

    commit_audit_table_ddl_sql

--- data/configs/audit_table_ddl.sql
+++ data/configs/audit_table_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE avd-databricks-demo.temp_dataset.audit_log (
+CREATE TABLE metal-vehicle-482506-n5.temp_dataset.audit_log (
     data_source STRING,
     tablename STRING,
     load_type STRING,
